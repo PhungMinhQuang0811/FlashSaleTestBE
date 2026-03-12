44e808dded7d343a8a9ec38c242e1e0a673506b0
Phung Minh Quang <quangpm23@fpt.com>
2026-03-12 17:44:48 +0700

    init spring boot, config docker compose

---
+++ src/main/java/com/mp/flashsale/FlashSaleApplication.java
@@ -0,0 +1,10 @@
+package com.mp.flashsale;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashSaleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FlashSaleApplication.class, args);
+    }
+}
